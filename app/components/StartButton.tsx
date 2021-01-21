eaf7a11c5c4910455bd17814bac224228c18e01c
ritheesh4 <ritheesh.4@gmail.com>
2021-01-21 13:05:30 +0530

    material ui buttons and inputs added on home and start goal screens

--- app/components/StartButton.tsx
+++ app/components/StartButton.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
-import styles from './StartButton.css';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '5rem',
+        height: '5rem',
+      },
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  })
+);
 
 export default function StartButton() {
-  return <div className={styles.start__button}>START</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Fab color="primary" aria-label="add">
+        START
+      </Fab>
+    </div>
+  );
 }
